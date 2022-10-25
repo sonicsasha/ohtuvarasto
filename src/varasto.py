0b916f8faf7e266a43187e3fffbc638e00d26134
Sasha Usoskin <sashausoskin@live.com>
2022-10-25 12:55:31 +0300

    FIX lint error

--- src/varasto.py
+++ src/varasto.py
@@ -4,7 +4,7 @@ class Varasto:
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
-            self.saldo = 0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+            self.saldo = 0.0
         elif alku_saldo <= tilavuus:
             # mahtuu
             self.saldo = alku_saldo
